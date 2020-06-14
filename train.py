b810bca702d3be898a4c6d0cedbd83ca6418f513
robot.ai <robot.ai.network@outlook.com>
2020-06-15 07:42:07 +0800

    update

--- train.py
+++ train.py
@@ -3,9 +3,19 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-
 import torchvision
 import torchvision.transforms as transforms
-
 import os
 import argparse
+
+from model import DummyModel
+from utils import string_to_list
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--device-ids', help='GPU ids assignment, e.g. [0,1,2,3,...]')
+args = parser.parse_args()
+
+
--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
 import os
 import sys
+import re
+
+def string_to_list(string):
+    if isinstance(string, str):
+        string = re.findall(r'\d+', string)
+        string = list(map(int, string))
+        return string
+    elif isinstance(string, list):
+        print(f'{string} is list, no need to change')
+        return string
